d0fbcd7dec94ca92d4a4c8867180c1483f3bc27b
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-29 22:55:22 +0900

    Feat : Post CRUD Database
    메모리 데이터베이스에 Post를 CRUD하는 코드 추가

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,6 +1,7 @@
 package db;
 
 import com.google.common.collect.Maps;
+import model.Post;
 import model.User;
 
 import java.util.Collection;
@@ -9,11 +10,19 @@ import java.util.Optional;
 
 public class Database {
     private static final Map<String, User> users = Maps.newHashMap();
+    private static final Map<Integer, Post> posts = Maps.newHashMap();
+    private static int cnt = 0;
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
     }
+    public static Collection<Post> findAllPosts(){
+        return posts.values();
+    }
 
+    public static void addPost(Post post) {
+        posts.put(++cnt, post);
+    }
     public static Optional<User> findUserById(String userId) {
         return Optional.ofNullable(users.get(userId));
     }
